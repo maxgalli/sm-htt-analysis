5a6e5468d075332b435929f314f7bdeb238e1ba3
maxgalli <massimiliano.galli.95@gmail.com>
2019-11-02 17:43:22 +0100

    Necessary changes to run locally (laptop)

--- utils/setup_cvmfs_sft.sh
+++ utils/setup_cvmfs_sft.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-LCG_RELEASE=95
-if uname -a | grep ekpdeepthought -q
-then
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-ubuntu1604-gcc54-opt/setup.sh
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-ubuntu1604-gcc54-opt/setup.sh
-elif uname -a | grep -E 'el7' -q
-then
-    LCG_RELEASE=96
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-centos7-gcc8-opt/setup.sh
-else
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_${LCG_RELEASE}/x86_64-slc6-gcc8-opt/setup.sh
-fi
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-ubuntu1804-gcc8-opt/setup.sh
