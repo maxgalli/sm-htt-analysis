6abadabb3cbbddd2b2c6db082b77db642cfaabe3
maxgalli <massimiliano.galli.95@gmail.com>
2019-11-07 13:15:37 +0100

    Added shell script for control region analysis that works like run_analysis.sh

---
+++ run_control_analysis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Error handling to ensure that script is executed from top-level directory of
+# this repository
+for DIRECTORY in shapes datacards combine plotting utils
+do
+    if [ ! -d "$DIRECTORY" ]; then
+        echo "[FATAL] Directory $DIRECTORY not found, you are not in the top-level directory of the analysis repository?"
+        exit 1
+    fi
+done
+
+# Clean-up workspace
+#./utils/clean.sh
+
+# Parse arguments
+ERA=$1               # options: 2016, 2017
+CHANNELS=${@:2}      # options: em, et, mt, tt
+
+# Create shapes of systematics
+./shapes/produce_control_shapes.sh $ERA $CHANNELS
+
+# Apply blinding strategy
+#./shapes/apply_blinding.sh $ERA
+
+# Convert shapes to synced format
+#./shapes/convert_to_synced_shapes.sh $ERA
+
+
+# added by myself
+exit
