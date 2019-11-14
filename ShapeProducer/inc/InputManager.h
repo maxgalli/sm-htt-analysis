4074330188a0cd600c80fcf343debaa6e8dda57d
maxgalli <massimiliano.galli.95@gmail.com>
2019-11-14 22:54:08 +0100

    [Analysis, C++] Class InputManager
    This class takes care of getting the input root files and
    creating the convenient TChains that will work as input for
    the RDataFrames

---
+++ ShapeProducer/inc/InputManager.h
@@ -0,0 +1,18 @@
+// ROOT headers
+#include "TTree.h"
+#include "TChain.h"
+
+class InputManager {
+
+    public:
+        // default constructor
+        InputManager
+        // constructor with strings passed as arguments
+        InputManager
+
+        AddFile();
+
+    private:
+        TChain m_chain;
+
+};
