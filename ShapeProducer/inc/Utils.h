7a06f0fef5e95b45cc15516b937e54dfb4ea712a
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-11-29 10:40:27 +0100

    [Analysis, C++] Added typedefs to handle huge lenght of type returned by filter; for now not used to do include/compilation error that need to be investigated

--- ShapeProducer/inc/Utils.h
+++ ShapeProducer/inc/Utils.h
@@ -1,7 +1,14 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <vector>
 #include "TDirectoryFile.h"
 
 typedef std::vector<std::string> stringvec;
 typedef std::vector<TDirectoryFile*> tdfvec;
+//typedef ROOT::RDF::RInterface<ROOT::Detail::RDF::RJittedFilter, void> interface;
+//typedef std::vector<interface> interfacevec;
+
+#endif
