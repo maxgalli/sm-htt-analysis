f0bfc09c8e51fa457920655fcb4fbc9111ea66ee
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-11-29 10:57:11 +0100

    [Analysis, Python] CutScheduler class works properly

--- ShapeProducer/shape_producer/CutScheduler.py
+++ ShapeProducer/shape_producer/CutScheduler.py
@@ -1,6 +1,33 @@
 import ROOT
-import cutstring
 
 class CutScheduler:
-    def __init__(self, name, dataframe, cuts = None, weights = None):
+    def __init__(self, dataframe, cuts = None, weights = None):
+        self._in_rdf = dataframe
+        self._cuts = cuts
 
+    @property
+    def in_rdf(self):
+        return self._in_rdf
+
+    @in_rdf.setter
+    def in_rdf(self, a_dataframe):
+        self._in_rdf = a_dataframe
+
+    @property
+    def cuts(self):
+        return self._cuts
+
+    @cuts.setter
+    def cuts(self, a_cuts):
+        self._cuts = a_cuts
+
+    def apply_cuts(self):
+        internal_rdf = self._in_rdf
+        for cut in self._cuts:
+            self._out_rdf = internal_rdf.Filter(cut.string)
+            internal_rdf = self.out_rdf
+        return self._out_rdf
+
+    @property
+    def out_rdf(self):
+        return self._out_rdf
